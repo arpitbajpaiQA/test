55bea429fd854993c799fa23c44e40058efc273b
Arpit Bajpai <arpitbajpai@qainfotech.com>
2017-06-30 09:43:12 +0530

    Update TestBoard.java

--- test/java/TestBoard.java
+++ test/java/TestBoard.java
@@ -1,47 +1,81 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.qainfotech.tap.training.snl.api.Board;
 import com.qainfotech.tap.training.snl.api.BoardModel;
 import com.qainfotech.tap.training.snl.api.GameInProgressException;
 import com.qainfotech.tap.training.snl.api.MaxPlayersReachedExeption;
+import com.qainfotech.tap.training.snl.api.NoUserWithSuchUUIDException;
 import com.qainfotech.tap.training.snl.api.PlayerExistsException;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
-import org.json.JSONObject;
-import org.testng.annotations.BeforeTest;
 
 public class TestBoard {
 	Board board;
-	BoardModel boardmodel ;
+	BoardModel boardModel;
+	UUID uuid;
 	Object playerObject;
-	JSONObject player = (JSONObject)playerObject;
-	
+	JSONObject player = (JSONObject) playerObject;
+
 	@BeforeTest
-	public void load_the_Board() throws FileNotFoundException, UnsupportedEncodingException, IOException 
-	{		board = new Board();
+	public void load_the_Board() throws FileNotFoundException, UnsupportedEncodingException, IOException {
+		board = new Board();
+		uuid=board.getUUID();
+		board=new Board(uuid);
+		}
+
+	@Test
+	public void adding_a_player()
+			throws IOException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption {
+		board.registerPlayer("Arpit");
+		board.registerPlayer("Ruchir");
+
+		board.getData();
 	}
-  @Test
-  public void adding_a_player()	throws IOException, PlayerExistsException, GameInProgressException,
-  MaxPlayersReachedExeption 
-  {		board.registerPlayer("Arpit");	
-        board.registerPlayer("Ruchir");	
-        board.getData();
-  }
-  @Test	public void adding_new_player_should_throw_PlayerExistsException() throws FileNotFoundException,
-  UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
-  MaxPlayersReachedExeption, IOException 
-  {		board.registerPlayer("Ruchir");	
-        board.getData();	
-  }	
-  @Test	
-  public void adding_new_player_should_throw_MaxPlayersReachedExeption()  throws FileNotFoundException,
-  UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException
-  {		board.registerPlayer("Vishnu");		
-        board.registerPlayer("Himanshu");		
-        board.getData();
-        }	
-  
+
+	@Test
+	public void adding_new_player_should_throw_AlreadyPlayerExistsExcetion()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException {
+		board.registerPlayer("Ruchir");
+		board.getData();
+	}
+
+	@Test
+	public void adding_new_player_should_throw_MaxPlayersReachedExeption()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException {
+		board.registerPlayer("himanshu");
+		board.registerPlayer("pandey");
+		board.registerPlayer("vishnu");
+	
+		board.getData();
+	}
+
+	@Test
+	public void deleting_the_player_removes_player() throws FileNotFoundException, 
+	UnsupportedEncodingException, NoUserWithSuchUUIDException{
+		
+		UUID uuid1 = (UUID) board.getData().getJSONArray("players").getJSONObject(2).get("uuid");
+		board.deletePlayer(uuid1);	
+	}
+	
+	@Test
+	public void deleting_the_player_throws_NoUserWithSuchUUIDException() throws FileNotFoundException,
+	UnsupportedEncodingException, NoUserWithSuchUUIDException{
+		board.deletePlayer(uuid);
+		
+	}
+		
+	
+	
+	
+	
+	
+	
+	
+	
 }
